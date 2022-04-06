b6bc97cd2f8df0ed7ba286bb7e801a13c8c40482
marianaesc <89614914+marianaesc@users.noreply.github.com>
2022-04-06 15:13:46 -0500

    Conversión de álbumes a botones

--- Assets/Scripts/paneles.cs
+++ Assets/Scripts/paneles.cs
@@ -9,6 +9,11 @@ public class paneles : MonoBehaviour
     public GameObject panel_inicial;
     public GameObject panel_strokes;
     public GameObject panel_regreso;
+    public GameObject panel_benee;
+    public GameObject panel_sodastereo;
+    public GameObject panel_radiohead;
+    public GameObject panel_foxygen;
+    public GameObject panel_fosterthepeople;
 
     public void panelcambiogeneros()
     {
@@ -29,6 +34,57 @@ public class paneles : MonoBehaviour
     {
         panel_strokes.SetActive(false);
     }
+
+    public void panelbenee()
+    {
+        panel_benee.SetActive(true);
+    }
+
+    public void regresobenee()
+    {
+        panel_benee.SetActive(false);
+    }
+    public void panelsodastereo()
+    {
+        panel_sodastereo.SetActive(true);
+    }
+
+    public void regresosodastereo()
+    {
+        panel_sodastereo.SetActive(false);
+    }
+
+    public void panelradiohead()
+    {
+        panel_radiohead.SetActive(true);
+    }
+
+    public void regresoradiohead()
+    {
+        panel_radiohead.SetActive(false);
+    }
+
+    public void panelfoxygen()
+    {
+        panel_foxygen.SetActive(true);
+    }
+
+    public void regresofoxygen()
+    {
+        panel_foxygen.SetActive(false);
+    }
+
+    public void panelfosterthepeople()
+    {
+        panel_fosterthepeople.SetActive(true);
+    }
+
+    public void regresofosterthepeople()
+    {
+        panel_fosterthepeople.SetActive(false);
+    }
+
+
 
 
 }
